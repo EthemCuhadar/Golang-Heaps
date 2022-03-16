7752b1b5f634939ea088b25192c0f3a28372739d
EthemCuhadar <iethemcuhadar@gmail.com>
2022-03-16 23:38:06 +0300

    add swap function

--- Heaps/main.go
+++ Heaps/main.go
@@ -13,3 +13,28 @@ func (h *Heap) Insert(key int){
 }
 
 // Extract - Eliminates the element which has the largest key in the heap.
+
+
+func (h *Heap) maxHeapifyUp(index int){
+	
+}
+
+// parent - Returns parent index of an element.
+func parent(i int) int{
+	return (i-1)/2
+}
+
+// left - Returns left child index of an element.
+func left(i int) int{
+	return 2*i + 1
+}
+
+// right - Return right child index of an element.
+func right(i int) int{
+	return 2*i + 2
+}
+
+// swap - Simply swaps two elements in array.
+func (h *Heap) swap(i1, i2 int){
+	h.array[i1], h.array[i2] = h.array[i2], h.array[i1]
+}
